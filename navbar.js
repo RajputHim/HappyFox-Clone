1e4dc237e34c41b8f03e8c2de5a89072762b4b48
Hartumpa <iambeastonn@gmail.com>
2022-07-23 12:12:31 +0530

    signin

---
+++ navbar.js
@@ -0,0 +1,14 @@
+// let ram=document.getElementById("cs")
+// document.getElementById("menu").style.visibility="hidden";
+// let count=0;
+// ram.addEventListener("click",()=>{
+ 
+//   count++;
+//   console.log(count)
+//   if(count%2===1){
+
+//     document.getElementById("menu").style.visibility="visible";
+//   }else{
+//     document.getElementById("menu").style.visibility="hidden";
+//   }
+// })
