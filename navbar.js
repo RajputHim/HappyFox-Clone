700e6711c8dce7142c1f56d2d8848489644c29e8
Hartumpa <iambeastonn@gmail.com>
2022-07-23 22:17:53 +0530

    ace

--- navbar.js
+++ navbar.js
@@ -1,14 +1,54 @@
-// let ram=document.getElementById("cs")
-// document.getElementById("menu").style.visibility="hidden";
-// let count=0;
-// ram.addEventListener("click",()=>{
- 
-//   count++;
-//   console.log(count)
-//   if(count%2===1){
-
-//     document.getElementById("menu").style.visibility="visible";
-//   }else{
-//     document.getElementById("menu").style.visibility="hidden";
-//   }
+let ram1=document.querySelector(".cs1")
+// console.log(ram1)
+// ram1.addEventListener("click",()=>{
+//   console.log("1")
 // })
+document.querySelector(".ram1").style.visibility="hidden";
+document.querySelector("#nav2").style.visibility="hidden"
+let count=0;
+ram1.addEventListener("click",()=>{
+ 
+  count++;
+  if(count%2===1){
+    document.querySelector("#nav2").style.visibility="hidden"
+    document.querySelector(".ram1").style.visibility="visible";
+    document.querySelector(".ram2").style.visibility="hidden";
+    document.querySelector(".ram3").style.visibility="hidden";
+  }else{
+    document.querySelector(".ram1").style.visibility="hidden";
+    document.querySelector("#nav2").style.visibility="visible"
+  }
+})
+
+let ram2=document.querySelector(".cs2")
+document.querySelector(".ram2").style.visibility="hidden";
+let c2=0;
+ram2.addEventListener("click",()=>{
+  c2++;
+  if(c2%2===1){
+    document.querySelector("#nav2").style.visibility="hidden"
+    document.querySelector(".ram2").style.visibility="visible";
+    document.querySelector(".ram1").style.visibility="hidden";
+    document.querySelector(".ram3").style.visibility="hidden";
+  }else{
+    document.querySelector(".ram2").style.visibility="hidden";
+    document.querySelector("#nav2").style.visibility="visible"
+  }
+})
+
+let ram3=document.querySelector(".cs3")
+document.querySelector(".ram3").style.visibility="hidden";
+let c3=0;
+ram3.addEventListener("click",()=>{
+ 
+  c3++;
+  if(c3%2===1){
+    document.querySelector("#nav2").style.visibility="hidden"
+    document.querySelector(".ram3").style.visibility="visible";
+    document.querySelector(".ram2").style.visibility="hidden";
+    document.querySelector(".ram1").style.visibility="hidden";
+  }else{
+    document.querySelector(".ram3").style.visibility="hidden";
+    document.querySelector("#nav2").style.visibility="visible"
+  }
+})
